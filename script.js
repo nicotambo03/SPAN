bb7464e668e3427d241d597b3f455077bbd94482
nicotambo03 <130360379+nicotambo03@users.noreply.github.com>
2024-02-26 14:07:21 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function translate() {
-    
+function translatePage() {
+    const elementsToTranslate = document.querySelectorAll('[data-translate]');
+    elementsToTranslate.forEach(element => {
+        const translatedText = element.getAttribute('data-translate');
+        element.setAttribute('data-translate', element.textContent);
+        element.textContent = translatedText;
+    });
 }
+
+const translateButton = document.getElementById('en');
+translateButton.addEventListener('click', translatePage);
