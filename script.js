59011b4bc799ad4e63b2bb36033e76dcfe996b70
punzoalessandro <156683674+punzoalessandro@users.noreply.github.com>
2024-02-27 20:13:10 +0100

    modifiche

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
+var isTrue = true;
 
-function translatePage() {
-    const elementsToTranslate = document.querySelectorAll('[data-translate]');
-    elementsToTranslate.forEach(element => {
-        const translatedText = element.getAttribute('data-translate');
-        element.setAttribute('data-translate', element.textContent);
-        element.textContent = translatedText;
+
+function changeButtonColor() {
+  var ita = document.getElementById("it");
+  var eng = document.getElementById("en");
+  if (isTrue) {
+    ita.style.backgroundColor = "#5C6B5F";
+    ita.style.color = "white"
+    eng.style.backgroundColor = "white";
+    eng.style.color = "black"
+  } else {
+    eng.style.backgroundColor = "#5C6B5F";
+    eng.style.color = "white"
+    ita.style.backgroundColor = "white";
+    ita.style.color = "black"
+  }
+}
+// Chiamata alla funzione per inizializzare il colore del pulsante
+changeButtonColor();
+
+
+function translate() {
+    document.getElementById('en').addEventListener('click', function () {
+        isTrue = false;
+        changeButtonColor();
+        
+    })
+    document.getElementById('it').addEventListener('click', function () {
+        isTrue = true;
+        changeButtonColor();
+    })
+    document.getElementById('en').addEventListener('click', function () {
+        var divsToTranslate = document.querySelectorAll('[data-translate]');
+        divsToTranslate.forEach(function(div) {
+            var originalHTML = div.innerHTML;
+            var translatedHTML = div.dataset.translate;
+            div.innerHTML = translatedHTML;
+            div.dataset.translate = originalHTML;
+        });
+    });
+
+    document.getElementById('it').addEventListener('click', function () {
+        var divsToTranslate = document.querySelectorAll('[data-translate]');
+        divsToTranslate.forEach(function(div) {
+            var originalHTML = div.dataset.translate;
+            var translatedHTML = div.innerHTML;
+            div.innerHTML = originalHTML;
+            div.dataset.translate = translatedHTML;
+        });
     });
 }
-
-const translateButton = document.getElementById('en');
-translateButton.addEventListener('click', translatePage);
+translate();
