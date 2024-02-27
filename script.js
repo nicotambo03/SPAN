1dd2b02f83b8abafcfbc34e6e26a03bd60b1b536
punzoalessandro <156683674+punzoalessandro@users.noreply.github.com>
2024-02-27 08:46:21 +0100

    sjuh

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function translatePage() {
+    const elementsToTranslate = document.querySelectorAll('[data-translate]');
+    elementsToTranslate.forEach(element => {
+        const translatedText = element.getAttribute('data-translate');
+        element.setAttribute('data-translate', element.textContent);
+        element.textContent = translatedText;
+    });
+}
+
+const translateButton = document.getElementById('en');
+translateButton.addEventListener('click', translatePage);
