aed666796b10e6dd871ad2111a4130dbb85128b8
punzoalessandro <156683674+punzoalessandro@users.noreply.github.com>
2024-02-28 15:02:01 +0100

    xdzfserh

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var isTrue = true;
-
+var address = document.getElementById("addr-text");
+var phone = document.getElementById("phone-text");
+var time = document.getElementById("time-text");
+var title = document.getElementById("title");
+var map = document.getElementById("mapemb");
+var img1 = document.getElementById("im1");
+var img2 = document.getElementById("im2");
+var img3 = document.getElementById("im3");
 
 function changeButtonColor() {
   var ita = document.getElementById("it");
@@ -20,75 +27,77 @@ function changeButtonColor() {
 changeButtonColor();
 
 
-function translate() {
-    document.getElementById('en').addEventListener('click', function () {
+function translatePage() {
+    var divsToTranslate = document.querySelectorAll('[data-translate]');
+    divsToTranslate.forEach(function(div) {
+        if (!isTrue) {
+            div.innerHTML = div.dataset.translate;
+        } else {
+            div.innerHTML = div.dataset.translateit;
+        }
+    });
+}
+translatePage()
+document.getElementById('en').addEventListener('click', function () {
+    if (isTrue) {
         isTrue = false;
         changeButtonColor();
-        
-    })
-    document.getElementById('it').addEventListener('click', function () {
+        translatePage();
+    }
+});
+
+document.getElementById('it').addEventListener('click', function () {
+    if (!isTrue) {
         isTrue = true;
         changeButtonColor();
-    })
-    document.getElementById('en').addEventListener('click', function () {
-        var divsToTranslate = document.querySelectorAll('[data-translate]');
-        divsToTranslate.forEach(function(div) {
-            var originalHTML = div.innerHTML;
-            var translatedHTML = div.dataset.translate;
-            div.innerHTML = translatedHTML;
-            div.dataset.translate = originalHTML;
-        });
-    });
-
-    document.getElementById('it').addEventListener('click', function () {
-        var divsToTranslate = document.querySelectorAll('[data-translate]');
-        divsToTranslate.forEach(function(div) {
-            var originalHTML = div.dataset.translate;
-            var translatedHTML = div.innerHTML;
-            div.innerHTML = originalHTML;
-            div.dataset.translate = translatedHTML;
-        });
-    });
-}
-function infocontentesetter() {
-    var address = document.getElementById("addr-text");
-    var phone = document.getElementById("phone-text");
-    var time = document.getElementById("time-text");
-    var title = document.getElementById("title");
-    var map = document.getElementById("mapemb");
+        translatePage();
+    }
+});
     document.getElementById("goito").addEventListener('click',function () {
-        translate();translate();
         document.getElementById("infos").style.display = "block"
-        title.innerHTML = "Pizzeria a Bologna Goito"
-        title.dataset.translate = "Bologna Goito"
+        if (isTrue) {
+            title.innerHTML = "Pizzeria a Bologna Goito"
+        }else{
+            title.innerHTML= "Bologna Goito"
+        }
         address.innerHTML = "<b>Bologna, Italia</b><br>Via Goito 3g"
         phone.innerHTML = "(+39) 051 262808"
-        time.innerHTML = "<b>Pranzo:</b><br>Sabato e domenica dalle 12 alle 15 <br><br><b>Cena:</b><br>Tutti i giorni dalle 19 alle 24"
+        time.dataset.translateit = "<b>Pranzo:</b><br>Sabato e domenica dalle 12 alle 15 <br><br><b>Cena:</b><br>Tutti i giorni dalle 19 alle 24"
         time.dataset.translate = "<b>Lunch:</b><br>Saturday and Sunday from 12 am to 3 pm<br><br><b>Dinner:</b><br>Every day from 7 pm to 12 pm"
         map.src = "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJe6c22fnVf0cRNpLm8CTLjho&key=AIzaSyAvQkO9_MgXl9mb_TQHjsnP_NqJfs3qYdQ"
+        img1.src = "./Assets/Images/Goito1.jpg"
+        img2.src = "./Assets/Images/Goito2.jpg"
+        img3.src = "./Assets/Images/Goito3.jpg"
     });
     document.getElementById("massarenti").addEventListener('click',function () {
-        translate();translate();
         document.getElementById("infos").style.display = "block"
-        title.innerHTML = "Pizzeria a Massarenti"
-        title.dataset.translate = "Bologna Massarenti"
+        if (isTrue) {
+            title.innerHTML = "Pizzeria a Bologna Massarenti"
+        }else{
+            title.innerHTML= "Bologna Massarenti"
+        }
         address.innerHTML = "<b>Bologna, Italia</b><br>Via Massarenti 91/C"
         phone.innerHTML = "(+39) 051 284 0081"
-        time.innerHTML = "<b>Pranzo:</b><br>Al momento non siamo aperti a pranzo<br><br><b>Cena:</b><br>Tutti i giorni dalle 19 alle 24"
+        time.dataset.translateit = "<b>Pranzo:</b><br>Al momento non siamo aperti a pranzo<br><br><b>Cena:</b><br>Tutti i giorni dalle 19 alle 24"
         time.dataset.translate = "<b>Lunch:</b><br>We are not opened at lunch at the moment<br><br><b>Dinner:</b><br>Every day from 7 pm to 12 pm"
         map.src = "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJvazotQgrfkcRYJghrp7nyxw&key=AIzaSyAvQkO9_MgXl9mb_TQHjsnP_NqJfs3qYdQ"
+        img1.src = "./Assets/Images/Massarenti1.jpg"
+        img2.src = "./Assets/Images/Massarenti2.jpg"
+        img3.src = "./Assets/Images/Massarenti3.jpg"
     });
     document.getElementById("laurabassi").addEventListener('click',function () {
-        translate();translate();
         document.getElementById("infos").style.display = "block"
-        title.innerHTML = "Pizzeria a Laura Bassi"
-        title.dataset.translate = "Bologna Laura Bassi"
+        if (isTrue) {
+            title.innerHTML = "Pizzeria a Laura Bassi"
+        }else{
+            title.innerHTML= "Bologna Laura Bassi"
+        }
         address.innerHTML = "<b>Bologna, Italia</b><br>Via Laura Bassi Verratti 1h"
         phone.innerHTML = "(+39) 051 281 5313"
-        time.innerHTML = "<b>Pranzo:</b><br>Tutti i giorni dalle 12 alle 15<br><br><b>Cena:</b><br>Tutti i giorni dalle 19 alle 24"
+        time.dataset.translateit = "<b>Pranzo:</b><br>Tutti i giorni dalle 12 alle 15<br><br><b>Cena:</b><br>Tutti i giorni dalle 19 alle 24"
         time.dataset.translate = "<b>Lunch:</b><br>Every day from 12 am to 3 pm<br><br><b>Dinner:</b><br>Every day from 7 pm to 12 pm"
         map.src = "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJj2pHGw8rfkcRV46gwJqxqAM&key=AIzaSyAvQkO9_MgXl9mb_TQHjsnP_NqJfs3qYdQ"
+        img1.src = "./Assets/Images/Laurabassi1.jpg"
+        img2.src = "./Assets/Images/Laurabassi2.jpg"
+        img3.src = "./Assets/Images/Laurabassi3.jpg"
     });
-}
-translate();
-infocontentesetter();
